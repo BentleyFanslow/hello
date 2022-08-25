c31eb677763b6d37f864938e2334f4b28c63fcb2
BentleyFanslow <68929537+BentleyFanslow@users.noreply.github.com>
2022-08-25 00:26:25 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Bentley Fanslow
+ *         bfanslow@unomaha.edu
+ * Date: 2022/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello I'm Bentley Fanslow\n");
+  printf("My Major is Electrical Engineering\n");
+
+  return 0;
+}
